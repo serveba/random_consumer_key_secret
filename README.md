fc41c50846c627c993a14ee803f008ecd6806d7c
Sergio Velasco <serveba@prot-os.com>
2016-02-29 12:52:46 +0100

    sha256 and readme.md

---
+++ README.md
@@ -0,0 +1,6 @@
+#random_consumer_key_secret
+
+### Oauth consumer secret and key random generation
+
+The aim of this project is generating unique random consumer keys (32 bytes) and consumer secrets (48 bytes)
+
--- index.js
+++ index.js
@@ -6,18 +6,19 @@ var randomSource = RandomBytes.urandom.getInstance();
 // we get a random 32 bytes buffer (from /dev/urandom)
 randomSource.getRandomBytes(32, function(buff) {
 
-  //console.log('buff: ' + buff);
+  
   var ts = new Date().getTime();
 
   // we concat the current timestamp in ms
   buff += ts;
+  console.log('buff: ' + buff);
 
   // we create a sha512 hash and retrieve the hex string
-  var hash = crypto.createHmac('sha512', buff).digest('hex');
+  var hash = crypto.createHmac('sha256', buff).digest('hex');
 
   // we encode in b64 for key & secret generation
   var b64Hash = new Buffer(hash).toString('base64');
-  //console.log('hash: ' + b64Hash);
+  console.log('hash: ' + b64Hash);
 
   var key = b64Hash.substring(0, 32);
   console.log('consumer key: ' + key);
